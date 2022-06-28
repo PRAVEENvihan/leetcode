a99bc98ca87eb4998b345fd87ae7d4c034f8d1fb
Praveen Kumar <55134998+PRAVEENvihan@users.noreply.github.com>
2022-06-28 09:48:35 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (37.07%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,32 +1,61 @@
+// class Solution {
+//     public void sortColors(int[] nums) {
+//         int o=0;
+//         int z=0;
+//         int t=0;
+//         for(int i=0;i<nums.length;i++){
+//             if(nums[i]==1)
+//                 o++;
+//                 if(nums[i]==0)
+//                     z++;
+//                     if(nums[i]==2)
+//                         t++;
+//         }
+//         for(int i=0;i<nums.length;i++){
+//             if(z==0){
+//                 if(o==0){
+//                     nums[i]=2;
+//                     t--;
+//                 }
+//                 else{
+//                     nums[i]=1;
+//                     o--;
+//                 }
+//             }
+//             else{
+//                 nums[i]=0;
+//                 z--;
+//             }
+//         }
+        
+//     }
+// }
 class Solution {
     public void sortColors(int[] nums) {
-        int o=0;
-        int z=0;
-        int t=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==1)
-                o++;
-                if(nums[i]==0)
-                    z++;
-                    if(nums[i]==2)
-                        t++;
+        if(nums.length == 1){
+            return;
         }
-        for(int i=0;i<nums.length;i++){
-            if(z==0){
-                if(o==0){
-                    nums[i]=2;
-                    t--;
-                }
-                else{
-                    nums[i]=1;
-                    o--;
-                }
+        int head = 0; // i
+        int mid  = 0; // j
+        int tail = nums.length - 1; // k
+        while( mid <= tail){
+            
+            if(nums[mid]>1){
+                int temp = nums[mid];
+                nums[mid] = nums[tail];
+                nums[tail] = temp;
+                tail --;
+            }
+            else if(nums[mid]<1){                
+                int temp = nums[head];
+                nums[head] = nums[mid];
+                nums[mid] = temp;
+                head ++;
+                mid ++;
             }
             else{
-                nums[i]=0;
-                z--;
+                mid ++;
             }
         }
-        
     }
 }
