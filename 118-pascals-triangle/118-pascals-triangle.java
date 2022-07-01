cd322459322aae66c5130865bea5dde1038f656e
Praveen Kumar <55134998+PRAVEENvihan@users.noreply.github.com>
2022-07-01 21:39:32 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (5.63%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.java
+++ 118-pascals-triangle/118-pascals-triangle.java
@@ -1,20 +1,40 @@
+// class Solution {
+//     public List<List<Integer>> generate(int numRows) {
+//         if (numRows == 0) return new ArrayList<>(0);
+//         List<List<Integer>> resultList = new ArrayList<>(numRows);
+//         int n = 0;
+//         while (n < numRows) {
+//             List<Integer> tempResult = new ArrayList<>(n);
+//             int prev = 1;
+//             tempResult.add(prev);
+//             for (int j = 1; j <= n; j++) {
+//                 int current = (prev * (n - j + 1)) / j;
+//                 prev = current;
+//                 tempResult.add(current);
+//             }
+//             resultList.add(tempResult);
+//             n++;
+//         }
+//         return resultList;
+//     }
+// }
+           
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        if (numRows == 0) return new ArrayList<>(0);
-        List<List<Integer>> resultList = new ArrayList<>(numRows);
-        int n = 0;
-        while (n < numRows) {
-            List<Integer> tempResult = new ArrayList<>(n);
-            int prev = 1;
-            tempResult.add(prev);
-            for (int j = 1; j <= n; j++) {
-                int current = (prev * (n - j + 1)) / j;
-                prev = current;
-                tempResult.add(current);
+         List<List<Integer>> result=new ArrayList<>();
+        List<Integer> list=null;
+        for(int i=0;i<numRows;i++){
+            list=new ArrayList<>();
+            int start=0;
+            for(int j=0;j<=i;j++){
+                if(j==0||j==i){
+                    list.add(1);
+                    continue;
+                }list.add(result.get(i-1).get(start)+result.get(i-1).get(start+1));
+                start++;
             }
-            resultList.add(tempResult);
-            n++;
+            result.add(list);
         }
-        return resultList;
+        return result;
     }
 }
