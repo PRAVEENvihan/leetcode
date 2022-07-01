45a3eea5360440a73c3da769e1947ec14282bc7b
Praveen Kumar <55134998+PRAVEENvihan@users.noreply.github.com>
2022-07-01 10:49:02 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (66.22%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -12,17 +12,36 @@
 //         return false;
 //     }
 // }
+// class Solution {
+//     public boolean searchMatrix(int[][] matrix, int target) {
+//       int i = 0, j = matrix[0].length - 1;
+//       while(i < matrix.length && j >= 0) {
+//         if(matrix[i][j] == target)
+//           return true;
+//         else if(matrix[i][j] > target)
+//           j --;
+//         else if(matrix[i][j] < target)
+//           i ++;
+//       }
+//       return false;
+//     }
+// }
+
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-      int i = 0, j = matrix[0].length - 1;
-      while(i < matrix.length && j >= 0) {
-        if(matrix[i][j] == target)
-          return true;
-        else if(matrix[i][j] > target)
-          j --;
-        else if(matrix[i][j] < target)
-          i ++;
-      }
-      return false;
+    public boolean searchMatrix(int[][] arr, int target) {
+        int r=arr.length;
+        int c=arr[0].length;
+        int left=0;
+        int right= r*c-1;
+        while(left<=right){
+            int midpoint=left+(right-left)/2;
+                    int midpoint_element=arr[midpoint/c][midpoint%c];
+
+            if(midpoint_element==target) return true;
+           
+            else if(target<midpoint_element) right=midpoint-1;
+             else if(target>midpoint_element) left=midpoint+1;
+        }
+        return false;
     }
 }
