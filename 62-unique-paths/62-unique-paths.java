c391fd5d4bcf695f858f80b832052796db129275
Praveen Kumar <55134998+PRAVEENvihan@users.noreply.github.com>
2022-07-05 13:02:24 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (13.62%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -11,15 +11,45 @@
 //         return downMove + rightMove;
 //     }
 // }
-class Solution {
+
+
+
+
+
+
+// class Solution {
+//     public int uniquePaths(int m, int n) {
+// 		int[][] dp=new int[m][n];
+//         for(int i = 0 ; i<m;i++){
+//             for(int j = 0 ; j < n ; j++){
+//                 if(i==0 || j==0) dp[i][j]=1;
+//                 else dp[i][j]=dp[i-1][j]+dp[i][j-1];
+//             }
+//         }
+//         return dp[m-1][n-1];
+//     }
+// }
+
+
+
+public class Solution {
     public int uniquePaths(int m, int n) {
-		int[][] dp=new int[m][n];
-        for(int i = 0 ; i<m;i++){
-            for(int j = 0 ; j < n ; j++){
-                if(i==0 || j==0) dp[i][j]=1;
-                else dp[i][j]=dp[i-1][j]+dp[i][j-1];
-            }
+        if(m == 1 || n == 1)
+            return 1;
+        m--;
+        n--;
+        if(m < n) {              // Swap, so that m is the bigger number
+            m = m + n;
+            n = m - n;
+            m = m - n;
         }
-        return dp[m-1][n-1];
+        long res = 1;
+        int j = 1;
+        for(int i = m+1; i <= m+n; i++, j++){       // Instead of taking factorial, keep on multiply & divide
+            res *= i;
+            res /= j;
+        }
+            
+        return (int)res;
     }
 }
