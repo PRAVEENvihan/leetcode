11c7b5a057607776b58ebb054ab95c542661e2ee
Praveen Kumar <55134998+PRAVEENvihan@users.noreply.github.com>
2022-07-07 11:00:49 +0530

    Time: 1 ms (85.41%), Space: 41.8 MB (14.89%) - LeetHub

---
+++ 191-number-of-1-bits/191-number-of-1-bits.java
@@ -0,0 +1,9 @@
+public class Solution {
+     public int hammingWeight(int n) {
+        int count = 0;
+        for(int i=0; i<32; i++){
+            count += (n >> i & 1) == 1 ? 1: 0;
+        }
+        return count;
+    }
+}
