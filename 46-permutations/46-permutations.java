74f295543aa85e9f137e6ec1423b2e06502884be
Praveen Kumar <55134998+PRAVEENvihan@users.noreply.github.com>
2022-07-30 16:43:01 +0530

    Time: 3 ms (47.27%), Space: 44.9 MB (39.95%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,28 +1,63 @@
+// class Solution {
+    
+//     private void recurPermute(int[] nums ,List<Integer> ds, List<List<Integer>> ans, boolean freq[] ){
+//         if(ds.size()==nums.length){
+//             ans.add(new ArrayList<>(ds));
+//             return;
+//         }
+//         for(int i=0;i<nums.length;i++){
+//             if(!freq[i]){
+//                 freq[i]=true;
+//                 ds.add(nums[i]);
+//                 recurPermute(nums,ds,ans,freq);
+//                 ds.remove(ds.size()-1);
+//                 freq[i]=false;
+//             }
+//         }
+//     }
+    
+    
+    
+//     public List<List<Integer>> permute(int[] nums) {
+//         List<List<Integer>> ans= new ArrayList<>();
+//         List<Integer> ds = new ArrayList<>();
+//         boolean freq[]=new boolean[nums.length];
+//         recurPermute(nums , ds , ans, freq);
+//         return ans;
+//     }
+// }
+
 class Solution {
     
-    private void recurPermute(int[] nums ,List<Integer> ds, List<List<Integer>> ans, boolean freq[] ){
-        if(ds.size()==nums.length){
+    public void recurPermute(int ind ,  List<List<Integer>> ans , int[] nums){
+        if(ind==nums.length){
+            List<Integer> ds = new ArrayList<>();
+            for(int i = 0;i<nums.length;i++){
+                ds.add(nums[i]);
+            }
             ans.add(new ArrayList<>(ds));
             return;
         }
-        for(int i=0;i<nums.length;i++){
-            if(!freq[i]){
-                freq[i]=true;
-                ds.add(nums[i]);
-                recurPermute(nums,ds,ans,freq);
-                ds.remove(ds.size()-1);
-                freq[i]=false;
-            }
+        for(int i = ind;i<nums.length;i++){
+            swap(i,ind,nums);
+            recurPermute(ind+1,ans,nums);
+            swap(i,ind,nums);
         }
+        
+    }
+    public void swap(int i , int j , int[] nums){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
     }
     
     
     
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans= new ArrayList<>();
-        List<Integer> ds = new ArrayList<>();
-        boolean freq[]=new boolean[nums.length];
-        recurPermute(nums , ds , ans, freq);
-        return ans;
-    }
+ public List<List<Integer>> permute(int[] nums) {
+      List<List<Integer>> ans= new ArrayList<>();
+     recurPermute(0,ans,nums);
+     return ans;
+     
+ }
+
 }
