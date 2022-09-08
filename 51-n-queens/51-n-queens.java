c525f2f97835fe86d6d6afcc91860c6c922fef80
Praveen Kumar <55134998+PRAVEENvihan@users.noreply.github.com>
2022-09-08 23:43:25 +0530

    Time: 5 ms (69.90%), Space: 45 MB (72.54%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -1,32 +1,60 @@
-public class Solution {
-    
-   public List<List<String>> solveNQueens(int n) {
-    List<List<String>> result = new ArrayList<List<String>>();
-    helper(result, new ArrayList<String>(), 0, new boolean[n], new boolean[2*n], new boolean[2*n], n);
-    return result;
-}
-
-private void helper(List<List<String>> result, List<String> board, int row, boolean[] cols, boolean[] d1, boolean[] d2, int n){
-    if (row == n) {
-        result.add(new ArrayList<String>(board));
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        char[][] board= new char[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j]='.';
+            }
+        }
+        List<List<String>> res = new ArrayList < List < String >> ();
+        dfs(0 , board ,res);
+        return res;
     }
-    for (int col=0; col<n; col++){
-        int id1 = col - row + n;
-        int id2 = col + row;
-        if (!cols[col] && !d1[id1] && !d2[id2]){
-            char[] r = new char[n];
-            Arrays.fill(r, '.');
-            r[col] = 'Q';
-            board.add(new String(r));
-            cols[col] = true;
-            d1[id1] = true;
-            d2[id2] = true;
-            helper(result, board, row+1, cols, d1, d2, n);
-            board.remove(board.size()-1);
-            cols[col] = false;
-            d1[id1] = false;
-            d2[id2] = false;
+    static void dfs(int col , char[][] board , List<List<String>> res){
+        if(col==board.length){
+            res.add(construct(board));
+            return ;
+        }
+        for(int row = 0 ; row<board.length;row++){
+            if(validate(board,row , col)){
+                board[row][col]='Q';
+                dfs(col+1,board,res);
+                  board[row][col]='.';
+            }
         }
     }
-}
+    static List<String> construct(char board[][]){
+        List<String> res = new ArrayList<>();
+         for (int i = 0; i < board.length; i++) {
+            String s = new String(board[i]);
+            res.add(s);
+        }
+        return res;
+    }
+    
+    static boolean validate(char[][] board, int row, int col) {
+        int dupRow=row;
+        int dupCol=col;
+        while(row >=0 && col >=0){
+            if(board[row][col]=='Q') return false;
+            row--;col--;
+        }
+        row=dupRow;
+        col=dupCol;
+        while (col >= 0) {
+            if (board[row][col] == 'Q') return false;
+            col--;
+        }
+
+        row = dupRow;
+        col = dupCol;
+        while (col >= 0 && row < board.length) {
+            if (board[row][col] == 'Q') return false;
+            col--;
+            row++;
+        }
+        return true;
+        
+    }
+    
 }
